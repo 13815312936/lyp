0b95c13e308021c72b7351c4ac0216efd0d00a91
13815312936 <59355139+13815312936@users.noreply.github.com>
2019-12-30 16:36:04 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	cin >> n;
+	int *p = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >> p[i];
+	for (int i = 0; i < n; i++)
+		if (p[i] < 60)
+			sum = sum + 400;
+	cout << sum;
+}
